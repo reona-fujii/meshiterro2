00f32ed9b39e6cc34ffc4e689662982d3611c7d9
EC2 Default User <ec2-user@ip-172-31-3-69.ec2.internal>
2020-09-28 04:02:30 +0000

    [finish]favorite

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,9 +2,24 @@ class PostCommentsController < ApplicationController
   
   
   def create
+    @post_comment = PostComment.new(post_image_params)
+    @post_comment.user_id = current_user.id
+    @post_image = PostImage.find(params[:post_image_id])
+    @post_comment.post_image_id = @post_image.id
+    @post_comment.save
+    redirect_to post_image_path(params[:post_image_id])
   end
   
   def destroy
+    @post_comment = PostComment.find(post_image.post_comment, post_comment)
+    @post_comment.destroy
+    redirect_to post_image_path(params[:post_image_id])
+  end
+  
+  private
+  def post_image_params
+    params.require(:post_comment).permit(:comment)
+  end
   
   
 end
